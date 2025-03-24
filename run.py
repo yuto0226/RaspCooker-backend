e93b66af71d43f800a3a51df93e652fd32f6caf1
yuto0226 <wayne71112@gmail.com>
2025-03-24 17:56:17 +0800

    Fix ignore error message when flask run

--- run.py
+++ run.py
@@ -2,11 +2,7 @@ from sys import exit
 from app import create_app
 from app.config import configs
 
-try:
-    app = create_app()
-
-except:
-    exit()
+app = create_app()
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
